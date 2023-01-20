f58955729c0adfecff7fe1b674398f7f19786e9a
an <ancabilloni@github.com>
2023-01-20 16:41:53 -0500

    check frequency support ethernet check and configurability for serial port and baudrate

--- check_msg_frequency.py
+++ check_msg_frequency.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 import time 
 from struct import pack, unpack
 import serial
 import sys, getopt
+import socket
 
 def crc24quick(crc, size, buffer):
     crctab = [0x00000000,0x01864CFB,0x038AD50D,0x020C99F6,0x0793E6E1,0x0615AA1A,0x041933EC,0x059F7F17,
@@ -17,27 +19,12 @@ def crc24quick(crc, size, buffer):
     
     return (crc & 0xFFFFFF)
 
-def check_frequency(argv):
+def check_frequency(msg_type, _port, _baudrate):
     """
     Pass in the message type # to check its arrival frequency
     For example: check_frequency(1005)
     """
-    ser = serial.Serial("/dev/ttyUSB0", baudrate=9600)
-    msg_type = 1005
-
-    try:
-        opts, args = getopt.getopt(argv, "hm:", ["mfile="])
-    except getopt.GetoptError as err:
-        print(err)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('./serial_read.py -m <message type>')
-            sys.exit()
-        elif opt in ("-m", "--mfile"):
-            msg_type = int(arg)
-
-    print("CHECK FREQUENCY FOR MSG TYPE {}".format(msg_type))
+    ser = serial.Serial(port=_port, baudrate=_baudrate)
 
     data = b''
     t = time.time()
@@ -87,5 +74,94 @@ def check_frequency(argv):
         data += dat
         t = time.time()
 
+def forward_correction_from_udp(msg_type):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", 54000))
+    # sock.settimeout(0.9)
+    data = b""
+    t = time.time()
+    t0 = time.time()
+    while True:
+        dat = sock.recv(1024)
+        # print(unpack(len(dat)*'B', dat))
+        if (time.time() - t) > 0.5:
+            print("====")
+            while (len(data) > 0):
+                # print("Raw len: ", len(data))
+                buffer = unpack(len(data)*'B', data)
+                # print(buffer)
+                i = 0
+                while (i < len(data) and buffer[i] != 0xD3):
+                    i += 1
+
+                if (i == len(data) ):
+                    data = b""
+                    # print("!!!! exceed limit")
+                    break
+
+                if buffer[i] == 0xD3:
+                    
+                    if (i+2) > len(data)-1:
+                        break
+
+                    if (buffer[i+1] & 0xFC == 0x00):
+                        size = ((buffer[i+1] & 3) << 8 | buffer[i+2]) + 6
+                        # print("Real size: ", size)
+                        if len(data[i::]) < size:
+                            # print("!!!!! Under size")
+                            break
+                        
+                        if (crc24quick(0x000000, size, buffer[i:i+size]) == 0x00):
+                            header = (buffer[3] << 4) | (buffer[4] >> 4)
+                            print(header)
+                            if header == msg_type:
+                                print("New {} msg arrive after {} sec".format(msg_type, time.time() - t0))
+                                t0 = time.time()
+                        else:
+                            # print(size, buffer[i:i+size])
+                            pass
+                        data = data[i+size::]
+                    else:
+                        data = data[i+1::]
+        data += dat
+        t = time.time()
+
 if __name__ == '__main__':
-    check_frequency(sys.argv[1:])
+    # check_frequency(sys.argv[1:])
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:p:b:m:", ["help", "interface=", "port=", "baudrate=", "mfile="])
+    except getopt.GetoptError as err:
+        print(err)
+
+    interface = "ethernet"
+    msg_type = 1005
+    baud_rate = 9600
+    port = "/dev/ttyUSB0"
+    for opt, arg in opts:
+        if opt in ('-h', "--help"):
+            print("Description: Tool to check on base station data frequency.")
+            print("To get help: ./serial_read.py -h OR ./serial_read.py --help")
+            print('Options: ./serial_read.py -i <interface type> -p <port name> -b <baud rate> -m <message type>')
+            print('  <interface type> : serial or ethernet. Default: ethernet.')
+            print('  <port name> : /dev/ttyUSB0, /dev/ttyACM0, etc. Default: /dev/ttyUSB0')
+            print('  <baud rate> : integer (9600, 115200,...). Default: 9600')
+            print('  <message type>: 1005, 1006, 1007, etc..Default: 1005')
+            sys.exit()
+        elif opt in ("-i", "--interface"):
+            interface = str(arg)
+        elif opt in ("-p", "--port"):
+            interface_name = arg
+        elif opt in ("-m", "--mfile"):
+            msg_type = int(arg)
+        elif opt in ("-b", "--baudrate"):
+            baud_rate = int(arg)
+
+    
+    if interface == "serial":
+        s = "serial"
+        print("CHECK FREQUENCY FOR MSG TYPE {} on serial at port {} with baudrate {}".format(msg_type, port, baud_rate))
+        check_frequency(msg_type, port, baud_rate)
+    else:
+        print("CHECK FREQUENCY FOR MSG TYPE {} on ethernet at port 5400.".format(msg_type))
+        forward_correction_from_udp(msg_type)
+    
--- readme.md
+++ readme.md
@@ -6,14 +6,20 @@ __Tested on Ubuntu 18.04__
 
 1. `check_msg_frequency.py`: use this to check specific message frequency
     __Hardware Requirement:__
-    - Connect USB-RS232 device from PC to base station radio or main board.
+    - If perform Serial interface check, connect USB-RS232 device from PC to base station radio or main board.
+    - If perform Ethernet interface check, join the network with IP 195.0.0.XX 
     
     __How to use__:
-    - Default message to check is __1005__ which is the main MSM message that client rover would need from base station. Simply run `python check_msg_frequency.py`
+    - Default message to check is __1005__ which is the main MSM message that client rover would need from base station. Simply run `python check_msg_frequency.py` to check on Ethernet or Wifi interface.
     Example:
     ![img](./img/check_frequency1.png)
 
-    - If you want to check other message instead. Run `python check_msg_frequency.py -m other_msg_type`
+    - If you want to check other message instead on Ethernet or Wifi. Run `python check_msg_frequency.py -m other_msg_type`
     Example:
     ![img2](./img/check_frequency2.png)
+    - If you want to check on Serial/RS-232 at default 9600 baudrate and interface `/dev/ttyUSB0`. Run `python check_msg_frequency.py -i serial`
+    Example:
+    ![img3](./img/check_frequency3.png)
 
+    - If you want to check on Serial/RS-232 at 115200 baudrate and interface `/dev/ttyUSB2` and message type 1004. Run `python check_msg_frequency.py -i serial -p /dev/ttyUSB2 -b 115200 -m 1004`
+    ![img4](./img/check_frequency4.png)
