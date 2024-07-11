bf8610b2493a616048ae95dc8361189e85d376c4
An Nguyen <anguyen@sealimited.com>
2024-07-11 15:28:29 -0400

    modify message read from unicast

--- check_msg_frequency.py
+++ check_msg_frequency.py
@@ -4,6 +4,7 @@ from struct import pack, unpack
 import serial
 import sys, getopt
 import socket
+import select 
 
 def crc24quick(crc, size, buffer):
     crctab = [0x00000000,0x01864CFB,0x038AD50D,0x020C99F6,0x0793E6E1,0x0615AA1A,0x041933EC,0x059F7F17,
@@ -75,56 +76,94 @@ def check_frequency(msg_type, _port, _baudrate):
         t = time.time()
 
 def forward_correction_from_udp(msg_type):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("", 54000))
+    multicast_group = ('234.5.6.7')
+
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # group = socket.inet_aton(multicast_group)
+    # sock.bind((multicast_group, 54008))
+    # mreq = pack("4s4s", socket.inet_aton(multicast_group), socket.inet_aton("195.0.0.217"))
+    # sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    # sock.setblocking(0)
+
+    # Unicast
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sock.bind(("", 54000))
     # sock.settimeout(0.9)
+
+    # for robot
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", 54008))
+
+    group = socket.inet_aton(multicast_group)
+    mreq = pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    sock.setblocking(0)
+
     data = b""
     t = time.time()
     t0 = time.time()
     while True:
-        dat = sock.recv(1024)
-        # print(unpack(len(dat)*'B', dat))
-        if (time.time() - t) > 0.5:
-            print("====")
-            while (len(data) > 0):
-                # print("Raw len: ", len(data))
-                buffer = unpack(len(data)*'B', data)
-                # print(buffer)
-                i = 0
-                while (i < len(data) and buffer[i] != 0xD3):
-                    i += 1
+        try:
+            ready = select.select([sock], [], [], 0.5)
+            if ready[0]:
+                dat = sock.recv(1024)
+            # print(len(dat))
+            # print(unpack(len(dat)*'B', dat))
+                if (time.time() - t) > 0.5:
+                    print("====")
+                    while (len(data) > 0):
+                        # print("Raw len: ", len(data))
+                        buffer = unpack(len(data)*'B', data)
+                        # print(buffer)
+                        i = 0
+                        while (i < len(data) and buffer[i] != 0xD3):
+                            i += 1
 
-                if (i == len(data) ):
-                    data = b""
-                    # print("!!!! exceed limit")
-                    break
-
-                if buffer[i] == 0xD3:
-                    
-                    if (i+2) > len(data)-1:
-                        break
-
-                    if (buffer[i+1] & 0xFC == 0x00):
-                        size = ((buffer[i+1] & 3) << 8 | buffer[i+2]) + 6
-                        # print("Real size: ", size)
-                        if len(data[i::]) < size:
-                            # print("!!!!! Under size")
+                        if (i == len(data) ):
+                            data = b""
+                            # print("!!!! exceed limit")
                             break
-                        
-                        if (crc24quick(0x000000, size, buffer[i:i+size]) == 0x00):
-                            header = (buffer[3] << 4) | (buffer[4] >> 4)
-                            print (time.asctime( time.localtime(time.time()) ) + (" , Message = ") + (str(header)))
-                            if header == msg_type:
-                                print(time.asctime( time.localtime(time.time()) ) + " , New {} msg arrive after {} sec".format(msg_type, time.time() - t0))
-                                t0 = time.time()
-                        else:
-                            # print(size, buffer[i:i+size])
-                            pass
-                        data = data[i+size::]
-                    else:
-                        data = data[i+1::]
-        data += dat
-        t = time.time()
+
+                        if buffer[i] == 0xD3:
+                            
+                            if (i+2) > len(data)-1:
+                                break
+
+                            if (buffer[i+1] & 0xFC == 0x00):
+                                size = ((buffer[i+1] & 3) << 8 | buffer[i+2]) + 6
+                                # print("Real size: ", size)
+                                if len(data[i::]) < size:
+                                    # print("!!!!! Under size")
+                                    break
+                                
+                                if (crc24quick(0x000000, size, buffer[i:i+size]) == 0x00):
+                                    header = (buffer[3] << 4) | (buffer[4] >> 4)
+                                    print (time.asctime( time.localtime(time.time()) ) + (" , Message = ") + (str(header)))
+                                    if header == msg_type:
+                                        print(time.asctime( time.localtime(time.time()) ) + " , New {} msg arrive after {} sec".format(msg_type, time.time() - t0))
+                                        t0 = time.time()
+                                else:
+                                    # print(size, buffer[i:i+size])
+                                    pass
+                                data = data[i+size::]
+                            else:
+                                data = data[i+1::]
+                data += dat
+                t = time.time()
+            else:
+                # print("no data, re init socket")
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.bind(("", 54008))
+
+                group = socket.inet_aton(multicast_group)
+                mreq = pack('4sL', group, socket.INADDR_ANY)
+                sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+                sock.setblocking(0)
+        except Exception as e:
+            print("An exception occurred")
+            print(e)
 
 if __name__ == '__main__':
     # check_frequency(sys.argv[1:])
